c80d1b404076bf038d6d37f8fd4f7bd039786d18
mosi <zxcvbnm914121@gmail.com>
2023-12-19 20:32:14 +0900

    init plus

--- launch/simple_display_1.launch.py
+++ launch/simple_display_1.launch.py
@@ -10,7 +10,7 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     urdf_tutorial_path = get_package_share_path('urdf_study')
-    default_model_path = urdf_tutorial_path / 'urdf/pan_tilt.urdf'
+    default_model_path = urdf_tutorial_path / 'urdf/myfirst.urdf'
     print("Default Model Path :", default_model_path)
     default_rviz_config_path = urdf_tutorial_path / 'rviz/urdf.rviz'
 
